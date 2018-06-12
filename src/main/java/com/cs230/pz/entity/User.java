c8f02ffae97a5d5395cb0a6ee39aad018d812b0e
nenadjevic2379 <nenad.jevtic.2379@metropolitan.ac.rs>
2018-06-12 18:09:08 +0200

    reg and auth

---
+++ src/main/java/com/cs230/pz/entity/User.java
@@ -0,0 +1,76 @@
+package com.cs230.pz.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Entity
+@NamedQueries({
+    @NamedQuery(name = "findUserById", query = "SELECT u FROM User u WHERE u.email = :email")
+})
+@Table(name = "users")
+public class User implements Serializable {
+
+    private static final long serialVersionUID = -5892169641074303723L;
+
+    @Id
+    @Column(name = "email", nullable = false, length = 255)
+    private String email;
+
+    @Column(name = "password", nullable = false, length = 64)
+    private String password;
+
+    @Column(name = "firstName", nullable = false, length = 30)
+    private String firstName;
+
+    @Column(name = "lastName", nullable = false, length = 30)
+    private String lastName;
+
+    public User() {
+    }
+
+    public User(String email, String password, String firstName, String lastName) {
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
---
+++ src/main/java/com/cs230/pz/managedbeans/RegisterView.java
@@ -0,0 +1,123 @@
+package com.cs230.pz.managedbeans;
+
+import java.io.Serializable;
+import java.util.logging.Logger;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ComponentSystemEvent;
+import javax.inject.Inject;
+
+import com.cs230.pz.ejb.UserEJB;
+import com.cs230.pz.entity.User;
+import javax.ejb.EJBException;
+
+@ManagedBean
+@SessionScoped
+public class RegisterView implements Serializable {
+
+    private static final long serialVersionUID = 1685823449195612778L;
+
+    private static Logger log = Logger.getLogger(RegisterView.class.getName());
+
+    @Inject
+    private UserEJB userEJB;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String confirmPassword;
+
+    public void validatePassword(ComponentSystemEvent event) {
+
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+
+        UIComponent components = event.getComponent();
+
+        // get password
+        UIInput uiInputPassword = (UIInput) components.findComponent("password");
+        String password = uiInputPassword.getLocalValue() == null ? "" : uiInputPassword.getLocalValue().toString();
+        String passwordId = uiInputPassword.getClientId();
+
+        // get confirm password
+        UIInput uiInputConfirmPassword = (UIInput) components.findComponent("confirmpassword");
+        String confirmPassword = uiInputConfirmPassword.getLocalValue() == null ? ""
+                : uiInputConfirmPassword.getLocalValue().toString();
+
+        // Let required="true" do its job.
+        if (password.isEmpty() || confirmPassword.isEmpty()) {
+            return;
+        }
+
+        if (!password.equals(confirmPassword)) {
+            FacesMessage msg = new FacesMessage("Confirm password does not match password");
+            msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+            facesContext.addMessage(passwordId, msg);
+            facesContext.renderResponse();
+        }
+
+        if (userEJB.findUserById(email) != null) {
+            FacesMessage msg = new FacesMessage("User with this e-mail already exists");
+            msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+            facesContext.addMessage(passwordId, msg);
+            facesContext.renderResponse();
+        }
+
+
+    }
+
+    public String register() {
+       
+        User user = new User(email, password, firstName, lastName);
+        userEJB.createUser(user);
+        log.info("New user created with e-mail: " + email + " and name: " + firstName + " " + lastName);
+        
+        
+        return "regdone";
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+}
